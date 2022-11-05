2e4736c2f9d7318a618eaf87f018e23439c26e4c
Artem Avetian <artemiustas@gmail.com>
2022-11-05 20:39:43 +0200

    added id, ts to log

--- master/master.py
+++ master/master.py
@@ -3,6 +3,10 @@ import sys, os, json, time, logging, requests
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from jsonschema import validate
+from tabulate import tabulate
+
+from multiprocessing.pool import ThreadPool
+import multiprocessing_logging
 
 def get_config(key):
     """
@@ -21,7 +25,7 @@ def replicate_msg(msg):
     for secondary_host in secondary_hosts:
         logging.info(f'Replicating the message on {secondary_host.get("name")} ({secondary_host.get("hostname")}:{secondary_host.get("port")})')
         api_url = f'https:// {secondary_host.get("hostname")}:{secondary_host.get("port")}'
-        #request_body = {"userId": 1, "title": "Buy milk", "completed": False}
+        request_body = {"userId": 1, "title": "Buy milk", "completed": False}
         response = requests.post(api_url, json=request_body)
         #print(response.json())
         #ack
@@ -33,63 +37,97 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info(f'{self.address_string()} requested list of messages')
         try:
-            dict_log_str = json.dumps(dict_log, indent=2)
-            response = b'List of messages:' + dict_log_str.encode('utf-8')
+            if log_list:
+                log_list_fmt = [ 
+                                    {
+                                        "id": msg.get("id"),
+                                        "msg": msg.get("msg"),
+                                        "created_ts" : datetime.utcfromtimestamp(msg.get("created_ts")).strftime("%Y-%m-%d %H:%M:%S.%f")
+                                    } 
+                                for msg in log_list 
+                                ]
+                log_list_str = tabulate(log_list_fmt, headers="keys")
+                response = log_list_str
+            else:
+                response= 'The replication log is empty'
             self.send_response(200)
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
             self.send_header('Server', 'Master')
             self.end_headers()            
-            self.wfile.write(response)
-            logging.info(f'Returned list of messages: {dict_log_str}')
+            self.wfile.write(response.encode('utf-8'))
+            logging.info(response)
         except Exception as e:
-            response = f"Exception: {e}".encode('utf-8')
+            response = f"Exception: {e}"
             self.send_response(500)
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
             self.send_header('Server', 'Master')
             self.end_headers()
-            self.wfile.write(response)     
+            self.wfile.write(response.encode('utf-8'))     
             logging.error(f"Exception: {e}", stack_info=debug)
 
     def do_POST(self):
         logging.info(f'{self.address_string()} sent a request to append message')
-        post_request_schema = {
-            "type": "object",
-            "properties": {
-                "name": {"msg": "string"},
-            },
-            "required": ["msg"],
-        }
-        
+                
         try:
             content_length = int(self.headers['Content-Length'])
             body = self.rfile.read(content_length).decode("utf-8")
+            body_dict = json.loads(body)
+            msg = body_dict.get("msg")
 
             #Validate input
             try:
-                validate(instance=json.loads(body), schema=post_request_schema)
-                response = f'Received message {body} has been added to log'.encode('utf-8')
+                post_request_schema = {
+                    "type": "object",
+                    "properties": {
+                        "name": {"msg": "string"},
+                    },
+                    "required": ["msg"],
+                }                
+                validate(instance=body_dict, schema=post_request_schema)
+
+                # get last message id
+                log_list_last_id = log_list[-1].get("id") if log_list else 0
+                # append new message to log
+                msg_id = log_list_last_id + 1
+                ts = time.time()
+                log_list.append({"id": msg_id, "msg": msg, "created_ts" : ts})
+                logging.info(f'Received message \"{msg}\" has been added to log with id: {msg_id}')
+                # trying to replicate message on every Secondary server
+                logging.info(f'Replicating message id = {msg_id}, msg = \"{msg}\" on ...')
+
+                response = f'Received message id = {msg_id}, msg = \"{msg}\" has been succesfully replicated'
                 self.send_response(200)
                 self.send_header('Content-Type', 'text/plain; charset=utf-8')
                 self.send_header('Server', 'Master')
                 self.end_headers()
-                self.wfile.write(response)
-                logging.info(f'Received message {body} has been added to log')    
+                self.wfile.write(response.encode('utf-8'))
+                logging.info(f'Received message id = {msg_id}, msg = \"{msg}\" has been succesfully replicated')
+
+                #if
+                #else
+                #response = f'Failed to replicate message: id = {msg_id}, msg = \"{msg}\"'
+                #self.send_response(599)
+                #self.send_header('Content-Type', 'text/plain; charset=utf-8')
+                #self.send_header('Server', 'Master')
+                #self.end_headers()
+                #self.wfile.write(response.encode('utf-8'))
+                #logging.info(f'Failed to replicate message: id = {msg_id}, msg = \"{msg}\"')
             except Exception as e:
-                response = f"Invalid POST request. Server accepts HTTP POST requests containing JSON of the following schema: {{\"msg\": \"message\"}}. Exception: {e}".encode('utf-8')                
+                response = f"Invalid POST request. Server accepts HTTP POST requests containing JSON of the following schema: {{\"msg\": \"message\"}}. Exception: {e}"               
                 self.send_response(400)
                 self.send_header('Content-Type', 'text/plain; charset=utf-8')
                 self.send_header('Server', 'Master')
                 self.end_headers()
-                self.wfile.write(response)
+                self.wfile.write(response.encode('utf-8'))
                 logging.error(f"Invalid POST request. Received message {body} has incorrect form. Exception: {e}", stack_info=debug)
         except Exception as e:
-            response = f"Exception: {e}".encode('utf-8')            
+            response = f"Exception: {e}"            
             self.send_response(500)
             self.send_header('Content-Type', 'text/plain; charset=utf-8')
             self.send_header('Server', 'Master')
             self.end_headers()
-            self.wfile.write(response)  
-            logging.error(f"Exception: {e}", stack_info=debug)
+            self.wfile.write(response.encode('utf-8'))  
+            logging.error(response, stack_info=debug)
 
     def log_message(self, format, *args):
         pass
@@ -105,7 +143,31 @@ def run_HTTP_server(server_class=HTTPServer, handler_class=SimpleHTTPRequestHand
 script_path = os.path.dirname(os.path.realpath(__file__))
 hosts = get_config("Hosts")
 secondary_hosts = list(filter(lambda host: host.get("type") == "secondary" and host.get("active") == 1, hosts))
-dict_log = {}
+
+log_list = []
+"""
+ts = time.time()
+log_list = [
+    {
+        "id": 1, 
+        "msg": "msg1", 
+        "created_ts": ts, 
+        "repl_status":
+        [
+            {
+                "host_id": "1", 
+                "ack": False,
+                "ack_ts": None
+            },
+            {
+                "host_id": "2", 
+                "ack": False,
+                "ack_ts": None
+            }
+        ]
+    }
+]
+"""
 
 """
 Main
@@ -127,6 +189,5 @@ if __name__ == '__main__':
     #logfile_name = datetime.now().strftime('master_%Y-%m-%d_%H-%M-%S.log')
     logfile_name = datetime.now().strftime('master.log')
     logfile_path = os.path.join(script_path, log_dir, logfile_name)
-    #%(funcName)s:%(lineno)d
     logging.basicConfig(filename=logfile_path, format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
     main()
