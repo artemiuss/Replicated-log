cd742d3efe73be87890ff9d8862379ab956ff414
Artem Avetian <artemiustas@gmail.com>
2022-12-26 23:18:52 +0200

    echo-server

--- echo-server.py
+++ echo-server.py
@@ -9,26 +9,30 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()    
-        response = "123"        
-        self.wfile.write(response.encode('utf-8'))
+        self.end_headers()  
+        response = BytesIO()        
+        response.write(b'This is GET request. ')
+        response.write(b'Original URL: ')
+        response.write(self.path.encode('utf-8'))
+        self.wfile.write(response.getvalue())
 
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')        
         self.end_headers()
         response = BytesIO()
         response.write(b'This is POST request. ')
-        response.write(b'Received: ')
+        response.write(b'Body: ')
         response.write(body)
         self.wfile.write(response.getvalue())
 
 
 
 def run_HTTP_server(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    master_port = "8080"
+    master_port = 8080
     httpd = HTTPServer(('', master_port), SimpleHTTPRequestHandler)
     httpd.serve_forever()
     
