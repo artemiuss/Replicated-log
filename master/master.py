d0a3abc613c68f530cd1faf604e23d61cb51155a
Artem Avetian <artemiustas@gmail.com>
2022-12-27 20:56:28 +0200

    replication added

--- master/master.py
+++ master/master.py
@@ -5,11 +5,8 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
 from jsonschema import validate
 from tabulate import tabulate
-
-from multiprocessing.pool import ThreadPool
-import multiprocessing_logging
-
 from threading import Lock
+import multiprocessing
 
 def get_config(key):
     """
@@ -21,17 +18,6 @@ def get_config(key):
             return dict_conf[key]
         except:
             raise
-
-def replicate_msg(msg):
-    logging.info(f'Replicating the message on every Secondary server')
-
-    for secondary_host in secondary_hosts:
-        logging.info(f'Replicating the message on {secondary_host.get("name")} ({secondary_host.get("hostname")}:{secondary_host.get("port")})')
-        api_url = f'https:// {secondary_host.get("hostname")}:{secondary_host.get("port")}'
-        request_body = {"userId": 1, "title": "Buy milk", "completed": False}
-        response = requests.post(api_url, json=request_body)
-        #print(response.json())
-        #ack
 
 """
 HTTP-server
@@ -54,12 +40,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                                     {
                                         "id": msg.get("id"),
                                         "msg": msg.get("msg"),
-                                        "created_ts" : datetime.utcfromtimestamp(msg.get("created_ts")).strftime("%Y-%m-%d %H:%M:%S.%f")
+                                        "replicated_ts" : datetime.utcfromtimestamp(msg.get("replicated_ts")).strftime("%Y-%m-%d %H:%M:%S.%f")
                                     } 
                                 for msg in log_list 
                                 ]
-                log_list_str = tabulate(log_list_fmt, headers="keys")
-                response = log_list_str
+                log_list_str = tabulate(log_list_fmt, headers="keys", tablefmt="simple_grid")
+                response = 'The replication log:\n' + log_list_str
             else:
                 response = 'The replication log is empty'
             self.send_response(200)
@@ -80,8 +66,20 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             logging.error(f"Exception: {e}", stack_info=debug)
 
     def do_POST(self):
-        # acquire the lock
-        self.lock.acquire()
+        def replicate_msg(secondary_host, msg_dict):
+            url = f'http://{secondary_host.get("hostname")}:{secondary_host.get("port")}'    
+            host_id = secondary_host.get("id")        
+            process = multiprocessing.current_process()
+
+            try:
+                response = requests.post(url, json=msg_dict, timeout=60)
+                repl_status_dict[secondary_host["id"]] = response.status_code
+                #if response.status_code == 200:
+                #    logging.info(f'[{process.pid}]: ...')
+                #else:
+                #    logging.info(f'[{process.pid}]: ...')    
+            except Exception as e:
+                logging.error(f"[{process.pid}]: Exception: {e}", stack_info=debug)
 
         logging.info(f'{self.address_string()} sent a request to append message')
         try:
@@ -98,39 +96,8 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                         "name": {"msg": "string"},
                     },
                     "required": ["msg"],
-                }                
+                }
                 validate(instance=body_dict, schema=post_request_schema)
-
-                # get last message id
-                log_list_last_id = log_list[-1].get("id") if log_list else 0
-                # append new message to log
-                msg_id = log_list_last_id + 1
-
-                ts = time.time()
-                log_list.append({"id": msg_id, "msg": msg, "created_ts" : ts})
-                logging.info(f'Received message \"{msg}\" has been added to log with id: {msg_id}')
-                # trying to replicate message on every Secondary server
-                logging.info(f'Replicating message id = {msg_id}, msg = \"{msg}\" on ...')
-                #replicate_msg()
-
-                response = f'Received message id = {msg_id}, msg = \"{msg}\" has been succesfully replicated'
-                self.send_response(200)
-                self.send_header('Content-Type', 'text/plain; charset=utf-8')
-                self.send_header('Server', 'Master')
-                self.end_headers()
-                response = response + '\n'                
-                self.wfile.write(response.encode('utf-8'))
-                logging.info(f'Received message id = {msg_id}, msg = \"{msg}\" has been succesfully replicated')
-
-                #if
-                #else
-                #response = f'Failed to replicate message: id = {msg_id}, msg = \"{msg}\"'
-                #self.send_response(599)
-                #self.send_header('Content-Type', 'text/plain; charset=utf-8')
-                #self.send_header('Server', 'Master')
-                #self.end_headers()
-                #self.wfile.write(response.encode('utf-8'))
-                #logging.info(f'Failed to replicate message: id = {msg_id}, msg = \"{msg}\"')
             except Exception as e:
                 response = f"Invalid POST request. Server accepts HTTP POST requests containing JSON of the following schema: {{\"msg\": \"message\"}}. Exception: {e}"               
                 self.send_response(400)
@@ -140,6 +107,44 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 response = response + '\n'
                 self.wfile.write(response.encode('utf-8'))
                 logging.error(f"Invalid POST request. Received message {body} has incorrect form. Exception: {e}", stack_info=debug)
+
+            # acquire the lock
+            self.lock.acquire()            
+            log_list_last_id = log_list[-1].get("id") if log_list else 0
+            msg_id = log_list_last_id + 1
+            msg_ts = time.time()
+            # append new message to log
+            msg_dict = {"id": msg_id, "msg": msg, "replicated_ts" : msg_ts}
+            log_list.append(msg_dict)
+            # release the lock
+            self.lock.release()                    
+            logging.info(f'Received message \"{msg}\" has been added to log with id: {msg_id}')
+
+            # trying to replicate message on every Secondary server
+            logging.info(f'Replicating the message on every Secondary server')
+            manager = multiprocessing.Manager()
+            repl_status_dict = manager.dict()
+            procs = []
+            for secondary_host in secondary_hosts:
+                p = multiprocessing.Process(target=replicate_msg, 
+                                            name="Replicating msg_id = " + str(msg_dict["id"]) +", msg = \"" + msg_dict["msg"] + "\" on " + secondary_host.get("name"),
+                                            args=(secondary_host, msg_dict))
+                procs.append(p)
+                p.start()
+                logging.info(f"START process [{p.pid}] {p.name}")
+            for proc in procs:
+                proc.join()
+                logging.info(f"END process [{proc.pid}] {proc.name}")
+
+            response = f'The message msg_id = {msg_id}, msg = \"{msg}\" has been succesfully replicated'
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain; charset=utf-8')
+            self.send_header('Server', 'Master')
+            self.end_headers()
+            response = response + '\n'
+            self.wfile.write(response.encode('utf-8'))
+            logging.info(response)   
+
         except Exception as e:
             response = f"Exception: {e}"
             self.send_response(500)
@@ -149,9 +154,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             response = response + '\n'
             self.wfile.write(response.encode('utf-8'))  
             logging.error(response, stack_info=debug)
-        finally:
-            # release the lock
-            self.lock.release()        
 
     def log_message(self, format, *args):
         pass
@@ -175,17 +177,15 @@ log_list = [
     {
         "id": 1, 
         "msg": "msg1", 
-        "created_ts": ts, 
+        "replicated_ts": ts, 
         "repl_status":
         [
             {
                 "host_id": "1", 
-                "ack": False,
                 "ack_ts": None
             },
             {
                 "host_id": "2", 
-                "ack": False,
                 "ack_ts": None
             }
         ]
@@ -221,3 +221,4 @@ if __name__ == '__main__':
         ]
     )
     main()
+   
